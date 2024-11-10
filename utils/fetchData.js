4fa6563f74b4e367721d7abf826a379703a935e6
kzaknsok <89708310+kzaknsok@users.noreply.github.com>
2024-11-10 15:28:49 +0900

    data.jsonへのパスを修正

--- utils/fetchData.js
+++ utils/fetchData.js
@@ -1,6 +1,6 @@
 async function fetchData() {
     try {
-        const response = await fetch('/data.json');
+        const response = await fetch('./data.json');
         return await response.json();
     } catch (error) {
         console.error('Error:', error);
